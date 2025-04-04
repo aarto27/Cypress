8aa95a4d454665fe99015d8b5796cefc62f041b9
aarto27 <sanchitsnarwat@gmail.com>
2025-04-04 12:35:42 +0530

    Added missing project files

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Implement node event listeners here
+    },
+    viewportHeight: 1100,
+    viewportWidth: 1920,
+  },
+});
