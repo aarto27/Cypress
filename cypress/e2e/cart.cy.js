284a489d324581e299811047989f67f0fd0c7486
aarto27 <sanchitsnarwat@gmail.com>
2025-04-25 10:51:19 +0530

    cypress

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -10,5 +10,9 @@ describe('cart', () => {
         cy.get(".lable").eq(1).click();
         cy.get(".tr-class").find(".btn-remove").eq(0).click();
         cy.get(".tr-class").find(".cart-qty-up").eq(0).click();
+        cy.get(".breadcrumb .mr-5").click();
+        cy.get(".wishlist_119252").eq(0)
+        .find(".fi-rs-heart")
+        .click({ force: true });
     });
 });
