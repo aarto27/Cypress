7b3608068a0fbc307542481c0dccfa3cb2e57bce
aarto27 <sanchitsnarwat@gmail.com>
2025-04-25 10:54:41 +0530

    Cypress

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -3,6 +3,9 @@ describe('cart', () => {
         cy.visit("https://hometown.zetupstore.com/");
         cy.get(".cart-trending_products_area-119254").click();
         cy.wait(2000);
+        cy.get(".wishlist_119252").eq(0)
+        .find(".fi-rs-heart")
+        .click({ force: true });
         cy.get(".cart-trending_products_area-119252").click();
         cy.wait(2000);
         cy.get(".cart-trending_products_area-119253").click();
@@ -11,8 +14,5 @@ describe('cart', () => {
         cy.get(".tr-class").find(".btn-remove").eq(0).click();
         cy.get(".tr-class").find(".cart-qty-up").eq(0).click();
         cy.get(".breadcrumb .mr-5").click();
-        cy.get(".wishlist_119252").eq(0)
-        .find(".fi-rs-heart")
-        .click({ force: true });
     });
 });
