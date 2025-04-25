4cdf7eb566506899f427d6e247782ae25f124a81
aarto27 <sanchitsnarwat@gmail.com>
2025-04-25 12:18:21 +0530

    Cypress

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -4,8 +4,10 @@ describe('cart', () => {
         cy.get(".cart-trending_products_area-119254").click();
         cy.wait(2000);
         cy.get(".wishlist_119252").eq(0)
-        .find(".fi-rs-heart")
-        .click({ force: true });
+            .find(".fi-rs-heart")
+            .click({
+                force: true
+            });
         cy.get(".cart-trending_products_area-119252").click();
         cy.wait(2000);
         cy.get(".cart-trending_products_area-119253").click();
@@ -19,5 +21,13 @@ describe('cart', () => {
         cy.get(".btn-remove").find(".fi-rs-trash").eq(0).click();
         cy.wait(2000);
         cy.get('.breadcrumb a').contains('Home').click();
+        cy.get(".toggle_info").click();
+        cy.get("#autocomplete")
+            .click()
+            .type("PVR, Mall of Jaipur, Gandhi Path West, B Block, Vaishali Nagar, Jaipur, Rajasthan, India")
+            .wait(1000)
+            .focused()
+            .type("{downarrow}{enter}");
+            cy.get(".confirm-btn").eq(0).click();
     });
 });
